1a1fc45ba24dde86e5f4d246855e6f3a73c2e132
Gary Woodfine <lnb0l9dc@duck.com>
2024-03-31 20:51:44 +0100

    Add unit and integration test projects
    This commit creates two separate projects for unit and integration tests. It includes basic xUnit setup, reference to Microsoft.NET.Test.Sdk and coverlets.collector packages. Both projects are included in the main solution file.

---
+++ tests/Integration/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Unit/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
